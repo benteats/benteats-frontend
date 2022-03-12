e28b197e40c8277b7a0c62d131c3acab5cbdf094
nachle <elias.nachle@bandtec.com.br>
2022-03-12 03:13:35 -0300

    feat: iniciando projeto react

---
+++ bents-projeto/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Texto from './Texto';
+
+function App() {
+  return (
+    <>
+      <Texto/> 
+    </>
+  );
+}
+
+export default App;
---
+++ projeto-site/src/Texto.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Texto(){
+    return(
+        <>
+            <h1>Texto 12</h1>
+        </>
+    )
+}
