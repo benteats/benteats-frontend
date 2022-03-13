0241c513f32bf68d88cc9ad60021204b072d0de1
nachle <elias.nachle@bandtec.com.br>
2022-03-13 19:52:09 -0300

    feat: implementando react routes e componente de Login

--- bents-app/src/App.tsx
+++ bents-app/src/App.tsx
@@ -1,9 +1,15 @@
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/home/Home'
+import Login from './pages/login/Login'
 
 export default function App() {
   return (
     <>
-      <Home />
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/' element={<Login/>}/>
+
+    </Routes>
     </>
   )
 }
