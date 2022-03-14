39fcfee86c0945a587dbab35fc6e7515f84aa1ef
felipe-galvao <felipe.galvao@bandtec.com.br>
2022-03-13 21:35:37 -0300

    FEAT(Home.tsx): Adicionando o componente de funcionalidades a página home

--- bents-app/src/pages/home/Home.tsx
+++ bents-app/src/pages/home/Home.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Hero from '../../components/home/hero/Hero'
 import Navbar from '../../components/home/navbar/Navbar'
 import Solution from '../../components/home/solution/Solution'
+import Funcionalities from '../../components/home/functionalities/Functionalities'
 
 export default function Home(){
     return(
     <>
         <Hero />
         <Solution />
+        <Funcionalities />
         <Navbar />
     </>
     )
