f3102ab01396182657596b25a7bfb06519f700aa
nachle <elias.nachle@bandtec.com.br>
2022-03-30 03:41:22 -0300

    feat: adicionando controle de paginas

--- bents-app/src/components/register/steps/UserAddress.tsx
+++ bents-app/src/components/register/steps/UserAddress.tsx
@@ -1,3 +1,28 @@
-export default function UserAddress() {
-  return <></>
+import * as FormStyle from '../../../styles/form/FormStyle'
+import * as StepsStyle from './Steps.style'
+
+export default function UserAddress({
+  page,
+  setPage
+}: {
+  page: any
+  setPage: any
+}) {
+  return (
+    <>
+      <StepsStyle.Container>
+        <StepsStyle.ContainerButton>
+          <FormStyle.PrevButton
+            type="button"
+            onClick={() => {
+              setPage((currPage: number) => currPage - 1)
+            }}
+          >
+            Anterior
+          </FormStyle.PrevButton>
+          <FormStyle.Button type="button">Concluir</FormStyle.Button>
+        </StepsStyle.ContainerButton>
+      </StepsStyle.Container>
+    </>
+  )
 }
--- bents-app/src/components/register/steps/UserData.tsx
+++ bents-app/src/components/register/steps/UserData.tsx
@@ -1,3 +1,35 @@
-export default function UserData() {
-  return <></>
+import * as FormStyle from '../../../styles/form/FormStyle'
+import * as StepsStyle from './Steps.style'
+
+export default function UserData({
+  page,
+  setPage
+}: {
+  page: any
+  setPage: any
+}) {
+  return (
+    <>
+      <StepsStyle.Container>
+        <StepsStyle.ContainerButton>
+          <FormStyle.PrevButton
+            type="button"
+            onClick={() => {
+              setPage((currPage: number) => currPage - 1)
+            }}
+          >
+            Anterior
+          </FormStyle.PrevButton>
+          <FormStyle.Button
+            type="button"
+            onClick={() => {
+              setPage((currPage: number) => currPage + 1)
+            }}
+          >
+            Próximo
+          </FormStyle.Button>
+        </StepsStyle.ContainerButton>
+      </StepsStyle.Container>
+    </>
+  )
 }
