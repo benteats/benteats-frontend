8d978e0f578d7b310918c548cc2b1f348dd43a29
nachle <elias.nachle@bandtec.com.br>
2022-03-30 03:29:16 -0300

    feat(StepsStyle): criando css dos steps de registro

---
+++ bents-app/src/components/register/steps/Steps.style.ts
@@ -0,0 +1,45 @@
+import { paletteColors, borderRadius, fontSize } from '../../../styles/Root'
+import styled from '@emotion/styled'
+
+export const Container = styled('div')`
+  padding-top: 2em;
+  display: grid;
+  gap: 1.75em;
+`
+
+export const Content = styled('div')``
+
+export const ContainerButton = styled('div')`
+  display: flex;
+  padding-top: 1.5em;
+`
+
+export const Checkbox = styled('div')`
+  display: grid;
+  grid-template-columns: 75px auto;
+  align-items: center;
+  gap: 2em;
+  padding: 1em 2em;
+  cursor: pointer;
+  height: 140px;
+  background: ${paletteColors.iptBackground};
+  border-radius: 35px;
+  svg {
+    height: 75px;
+    width: 75px;
+    padding: ${borderRadius.btn};
+    font-size: 70px;
+    border-radius: ${borderRadius.checkboxIcon};
+    background: ${paletteColors.white};
+    color: ${paletteColors.primary};
+  }
+  h2 {
+    padding-bottom: 0.25rem;
+    font-weight: 600;
+    font-size: 1.5em;
+  }
+  span {
+    color: ${paletteColors.secondaryText};
+    font-size: ${fontSize.mdText};
+  }
+`
