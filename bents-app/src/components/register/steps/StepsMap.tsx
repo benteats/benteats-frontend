c7fc3c48a1c68fdc76df6024c535bd66c123d426
nachle <elias.nachle@bandtec.com.br>
2022-03-30 03:29:38 -0300

    Feat(StepsMap): criando map para steps

---
+++ bents-app/src/components/register/steps/StepsMap.tsx
@@ -0,0 +1,17 @@
+import { FaUserFriends } from 'react-icons/fa'
+import { IoRestaurant } from 'react-icons/io5'
+
+export const userType = [
+  {
+    icon: <FaUserFriends />,
+    title: 'Usuário',
+    description:
+      'Você poderá compartihar suas críticas e descobrir lugares incríveis!'
+  },
+  {
+    icon: <IoRestaurant />,
+    title: 'Restaurante',
+    description:
+      'Venha descobrir como anda seu empreendimento através da visão de quem mais importa.'
+  }
+]
