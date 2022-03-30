ebd1dec287a1a9bf97daa849935be87fcbe42915
nachle <elias.nachle@bandtec.com.br>
2022-03-30 03:30:00 -0300

    feat(UserType): criando tela de tipo do usuario

--- bents-app/src/components/register/steps/UserType.tsx
+++ bents-app/src/components/register/steps/UserType.tsx
@@ -1,3 +1,37 @@
-export default function UserType() {
-  return <></>
+import * as FormStyle from '../../../styles/form/FormStyle'
+import * as StepsStyle from './Steps.style'
+import { userType } from './StepsMap'
+
+export default function UserType({
+  page,
+  setPage
+}: {
+  page: any
+  setPage: any
+}) {
+  return (
+    <>
+      <StepsStyle.Container>
+        {userType.map((element, item) => (
+          <StepsStyle.Checkbox key={item}>
+            {element.icon}
+            <StepsStyle.Content>
+              <h2>{element.title}</h2>
+              <span>{element.description}</span>
+            </StepsStyle.Content>
+          </StepsStyle.Checkbox>
+        ))}
+        <StepsStyle.ContainerButton>
+          <FormStyle.Button
+            type="button"
+            onClick={() => {
+              setPage((currPage: number) => currPage + 1)
+            }}
+          >
+            Próximo
+          </FormStyle.Button>
+        </StepsStyle.ContainerButton>
+      </StepsStyle.Container>
+    </>
+  )
 }
