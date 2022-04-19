13c109f4ed49e16e18b57468eca550c21d7296d7
eliasnachle <elias.nachle@bandtec.com.br>
2022-04-19 17:30:50 -0300

    feat: adicionando dados as classes

--- bents-cadastro/src/main/java/bents/bentscadastro/user/controller/UserController.java
+++ bents-cadastro/src/main/java/bents/bentscadastro/user/controller/UserController.java
@@ -1,4 +1,33 @@
 package bents.bentscadastro.user.controller;
 
+import bents.bentscadastro.user.entity.User;
+import bents.bentscadastro.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins="http://localhost:3000")
+@RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private UserRepository repository;
+
+    @PostMapping
+    public ResponseEntity postUser(@RequestBody @Valid User newUser) {
+        repository.save(newUser);
+        return ResponseEntity.status(201).build();
+    }
+
+    @GetMapping
+    public ResponseEntity getUsers() {
+        List<User> users = repository.findAll();
+        if (users.isEmpty()) {
+            return ResponseEntity.status(204).build();
+        }
+        return ResponseEntity.status(200).body(users);
+    }
 }
--- bents-cadastro/src/main/java/bents/bentscadastro/user/repository/UserRepository.java
+++ bents-cadastro/src/main/java/bents/bentscadastro/user/repository/UserRepository.java
@@ -1,2 +1,9 @@
-package bents.bentscadastro.user.repository;public interface UserRepository {
+package bents.bentscadastro.user.repository;
+
+import bents.bentscadastro.client.Client;
+import bents.bentscadastro.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
