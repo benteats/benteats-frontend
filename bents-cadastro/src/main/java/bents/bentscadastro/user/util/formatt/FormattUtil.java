e73890526e6467c999e05a43bbdc5bc3da2cae77
nachle <elias.nachle@bandtec.com.br>
2022-04-23 19:25:21 -0300

    feat(FormatUtill): adicionando replaceAll para limpar caracteres especiais

--- bents-cadastro/src/main/java/bents/bentscadastro/user/util/formatt/FormattUtil.java
+++ bents-cadastro/src/main/java/bents/bentscadastro/user/util/formatt/FormattUtil.java
@@ -4,6 +4,7 @@ import javax.swing.text.MaskFormatter;
 
 public class FormattUtil {
     public static String formattPhone(String phone) throws java.text.ParseException {
+        phone = phone.replaceAll("[-+.^:() ,]","");
         String maskPhone = "(AA) AAAAA-AAAA";
         if(phone.startsWith("0")) phone = phone.substring(1);
         MaskFormatter mf = new MaskFormatter(maskPhone);
