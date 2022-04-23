65dff2dc4986331a5642c4693a08afd4cfa2406d
nachle <elias.nachle@bandtec.com.br>
2022-04-23 19:25:36 -0300

    refactor: removendo metodo dentro do teste

--- bents-cadastro/src/test/java/bents/bentscadastro/user/util/FormattUtilTest.java
+++ bents-cadastro/src/test/java/bents/bentscadastro/user/util/FormattUtilTest.java
@@ -3,19 +3,12 @@ package bents.bentscadastro.user.util;
 import javax.swing.text.MaskFormatter;
 import java.text.ParseException;
 
+import static bents.bentscadastro.user.util.formatt.FormattUtil.formattPhone;
+
 public class FormattUtilTest {
     public static void main(String[] args) throws ParseException {
-        String phone = "11971145076";
+        String phone = "11912345678";
         String formattedPhone = formattPhone(phone);
         System.out.println(formattedPhone);
         }
-
-    public static String formattPhone(String phone) throws java.text.ParseException {
-        phone = phone.replaceAll("[-+.^:() ,]","");
-        String maskPhone = "(AA) AAAAA-AAAA";
-        if(phone.startsWith("0")) phone = phone.substring(1);
-        MaskFormatter mf = new MaskFormatter(maskPhone);
-        mf.setValueContainsLiteralCharacters(false);
-        return mf.valueToString(phone);
-    }
 }
