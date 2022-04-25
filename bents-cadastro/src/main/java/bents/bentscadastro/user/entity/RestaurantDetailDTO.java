fc5f01654185c99333bc92f4998f0139ae271e44
nachle <elias.nachle@bandtec.com.br>
2022-04-25 02:51:11 -0300

    feat: criando metodo de busca de restaurantes pelas coordenadas

---
+++ bents-cadastro/src/main/java/bents/bentscadastro/user/entity/RestaurantDetailDTO.java
@@ -0,0 +1,10 @@
+package bents.bentscadastro.user.entity;
+
+public interface RestaurantDetailDTO {
+    String getName();
+    String getAddress();
+    String getAddressNumber();
+    String getFoodType();
+    String getPriceAverage();
+    String getOpeningTime();
+}
--- bents-cadastro/src/main/java/bents/bentscadastro/user/repository/RestaurantRepository.java
+++ bents-cadastro/src/main/java/bents/bentscadastro/user/repository/RestaurantRepository.java
@@ -1,11 +1,14 @@
 package bents.bentscadastro.user.repository;
 
 import bents.bentscadastro.user.entity.Restaurant;
+import bents.bentscadastro.user.entity.RestaurantDetailDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Integer> {
 
@@ -46,4 +49,14 @@ public interface RestaurantRepository extends JpaRepository<Restaurant, Integer>
     @Modifying
     @Query("update Restaurant r set r.isLogged = true where r.email = ?1 or r.phone = ?1 and r.password = ?2")
     void loginUser(String login, String password);
+
+    String HAVERSINE_FORMULA = "(6371 * acos(cos(radians(:lat)) * cos(radians(r.lat)) *" +
+            " cos(radians(r.lng) - radians(:lng)) + sin(radians(:lat)) * sin(radians(r.lat))))";
+    @Query("SELECT r.name as name," +
+            "r.address as address," +
+            "r.addressNumber as addressNumber," +
+            " r.foodType as foodType," +
+            "r.priceAverage as priceAverage," +
+            "r.openingTime as openingTime FROM Restaurant r WHERE " + HAVERSINE_FORMULA + " < :distance ORDER BY "+ HAVERSINE_FORMULA + " DESC")
+    List<RestaurantDetailDTO> findRestaurantsWithInDistance(@Param("lat") double lat, @Param("lng") double lng, @Param("distance") double distanceWithInKM);
 }
