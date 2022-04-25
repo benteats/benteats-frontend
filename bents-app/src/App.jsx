6d70f011bfeab2df6a053b8a66a7185e4d1f9f75
nachle <elias.nachle@bandtec.com.br>
2022-04-25 18:26:44 -0300

    feat: implementando  tela 404 as rotas

--- bents-app/src/App.jsx
+++ bents-app/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import NotFound from './pages/notFound/NotFound'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import Register from './pages/register/Register'
@@ -13,6 +14,7 @@ export default function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/registro" element={<Register />} />
           <Route path="/searchRestaurant" element={<SearchRestaurant />} />
+          <Route path="/*" element={<NotFound />} />
         </Routes>
       </Router>
     </>
