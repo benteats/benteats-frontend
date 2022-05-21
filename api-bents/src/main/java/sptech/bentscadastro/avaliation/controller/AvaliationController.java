5ae4fb5719d941368c1e58812af9783aa474e933
nachle <elias.nachle@bandtec.com.br>
2022-05-21 20:45:03 -0300

    refactor: unificando apenas frontend no repositorio

--- api-bents/src/main/java/sptech/bentscadastro/avaliation/controller/AvaliationController.java
+++
@@ -1,9 +0,0 @@
-package sptech.bentscadastro.avaliation.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/avaliations")
-public class AvaliationController {
-}
--- api-bents/src/main/java/sptech/bentscadastro/avaliation/repository/AvaliationRepository.java
+++
@@ -1,7 +0,0 @@
-package sptech.bentscadastro.avaliation.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sptech.bentscadastro.avaliation.entity.Avaliation;
-
-public interface AvaliationRepository extends JpaRepository<Avaliation, Integer> {
-}
