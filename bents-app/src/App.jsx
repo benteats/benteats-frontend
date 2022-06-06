9b1786f0772d78866fe527b1a2920e85e88bc858
nachle <elias.nachle@bandtec.com.br>
2022-06-05 22:21:16 -0300

    feat(App.jsx): criando protecao de rota

--- bents-app/src/App.jsx
+++ bents-app/src/App.jsx
@@ -1,5 +1,5 @@
 import { useLayoutEffect, useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import NotFound from './pages/notFound/NotFound'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
@@ -7,6 +7,11 @@ import Register from './pages/register/Register'
 import SearchRestaurant from './pages/searchRestaurant/SearchRestaurant'
 import AuthProvider from './context/AuthContext'
 import history from './history'
+
+const PrivateRoute = ({ children, redirectTo }) => {
+  const isAuthenticated = localStorage.getItem('token') !== null
+  return isAuthenticated ? children : <Navigate to={redirectTo} />
+}
 
 const CustomRouter = ({ history, ...props }) => {
   const [state, setState] = useState({
@@ -35,7 +40,11 @@ export default function App() {
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/registro" element={<Register />} />
-            <Route path="/restaurantes" element={<SearchRestaurant />} />
+            <Route path="/restaurantes" element={
+              <PrivateRoute redirectTo="/login">
+                <SearchRestaurant />
+              </PrivateRoute>
+            } />
             <Route path="/*" element={<NotFound />} />
           </Routes>
         </CustomRouter>
