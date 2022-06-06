6c4e60b3c4d5d4788635ed73c9784296db1ed42b
nachle <elias.nachle@bandtec.com.br>
2022-06-05 22:14:32 -0300

    feat(AuthContext): criando context

--- bents-app/src/context/AuthContext.jsx
+++ bents-app/src/context/AuthContext.jsx
@@ -1,32 +1,13 @@
-import { createContext, useState } from 'react';
-import { URL_AZURE } from '../constants/http.azure.request'
-import axios from 'axios'
-import { MdOutlineError } from 'react-icons/md'
+import { createContext } from 'react'
+import useAuth from './hooks/UseAuth';
 
 export const Context = createContext()
 
 export default function AuthProvider({ children }) {
-  const [authenticated, setAuthenticated] = useState(false);
-
-  async function handleLogin(formLogin, { setErrorPostUser }) {
-    try {
-      const { data: { token } } = await axios.post(`${URL_AZURE}/login`, formLogin);
-      console.log(JSON.stringify(token))
-      console.log(data)
-      // localStorage.setItem('token', JSON.stringify(token))
-    } catch (e) {
-      console.error('error postUser =>', e)
-      setErrorPostUser(
-        <>
-          <MdOutlineError />
-          Ops! Algum dos dados estão inválidos.
-        </>
-      )
-    }
-  }
+  const { authenticated, loading, handleLogin, handleLogout } = useAuth();
 
   return (
-    <Context.Provider value={{ authenticated, handleLogin }} >
+    <Context.Provider value={{ authenticated, loading, handleLogin, handleLogout }} >
       {children}
     </Context.Provider>
   )
