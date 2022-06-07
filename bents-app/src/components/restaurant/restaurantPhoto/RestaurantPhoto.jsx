0beb057c235d069f19322c1f0a7b97d2da289f21
nachle <elias.nachle@bandtec.com.br>
2022-06-07 01:53:13 -0300

    feat(RestaurantPhoto): alterando estrutura do componente de foto

--- bents-app/src/components/restaurant/restaurantPhoto/RestaurantPhoto.jsx
+++ bents-app/src/components/restaurant/restaurantPhoto/RestaurantPhoto.jsx
@@ -8,22 +8,18 @@ export default function RestaurantPhoto() {
           <img src="https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" />
         </RestaurantStyle.PrincipalPhoto>
         <RestaurantStyle.OtherPhotos>
-          <div>
             <div>
               <img src="https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" />
             </div>
             <div>
               <img src="https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" />
             </div>
-          </div>
-          <div>
             <div>
               <img src="https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" />
             </div>
             <div>
               <img src="https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" />
             </div>
-          </div>
         </RestaurantStyle.OtherPhotos>
       </RestaurantStyle.PhotosContent>
     </>
--- bents-app/src/components/restaurant/restaurantPhoto/RestaurantPhoto.style.js
+++ bents-app/src/components/restaurant/restaurantPhoto/RestaurantPhoto.style.js
@@ -2,18 +2,14 @@ import { paletteColors, borderRadius, fontSize } from '../../../styles/Root'
 import styled from '@emotion/styled'
 
 export const PhotosContent = styled('div')`
-  max-width: 1680px;
   width: 100%;
-  height: 50vh;
+  min-height: 400px;
+  height: 100%;
   display: flex;
   gap: 1em;
-  @media screen and (max-width: 1080px){
-    grid-template-columns: 45% 55%;
-  }
 `
 
 export const PrincipalPhoto = styled('div')`
-  height: 100%;
   width: 100%;
   img{
     border-radius: 1.5em 0 0 1.5em;
@@ -24,33 +20,28 @@ export const PrincipalPhoto = styled('div')`
 `
 
 export const OtherPhotos = styled('div')`
+  min-height: 400px;
   height: 100%;
   width: 100%;
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
+  grid-template-columns: repeat(2, 20em);
   grid-gap: 1em;
   div{
-    display: grid;
-    grid-gap: 1em;
     height: 100%;
-    &:nth-of-type(2){
-      div:nth-of-type(1){
-        img{
-          border-radius: 0 1em 0 0;
-        }
-      }
-      div:nth-of-type(2){
-        img{
-          border-radius: 0 0 1em 0;
-        }
-      }
-    }
-    div{
       img{
         height: 100%;
         width: 100%;
         object-fit: cover;
       }
-    }
+      &:nth-of-type(2){
+        img{
+          border-radius: 0 1em 0 0;
+        }
+      }
+      &:nth-of-type(4){
+        img{
+          border-radius: 0 0 1em 0;
+        }
+      }
   }
 `
