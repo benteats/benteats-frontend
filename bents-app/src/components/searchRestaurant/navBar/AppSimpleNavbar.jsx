b56f55713925ccac5f55b2e745c1f0c546d5c1a9
nachle <elias.nachle@bandtec.com.br>
2022-06-07 01:54:21 -0300

    feat(AppSimpleNavbar.jsx): criando componente de navbar para tela do restaurante

---
+++ bents-app/src/components/searchRestaurant/navBar/AppSimpleNavbar.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react'
+import { ReactComponent as Logo } from '../../../assets/logo.svg'
+import { ReactComponent as AvatarSVG } from '../../../assets/avatar.svg'
+import { GrMenu } from 'react-icons/gr'
+import * as NavbarStyle from './AppNavBar.style'
+import ProfileMenu from './profileMenu/ProfileMenu'
+
+function reizeWindowSize() {
+  const [windowWidthSize, setWindowWidthSize] = useState(undefined)
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      function handleResize() {
+        setWindowWidthSize(window.innerWidth)
+      }
+      window.addEventListener('resize', handleResize)
+      handleResize()
+      return () => window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+  return windowWidthSize
+}
+
+export default function AppSimpleNavbar() {
+    const widthSizeScreen = reizeWindowSize()
+    const [isOpen, setOpen] = useState(false)
+    const toggleProfileMenu = () => {
+      setOpen(!isOpen)
+    }
+
+    return (
+      <>
+        <NavbarStyle.Nav>
+          <NavbarStyle.ContainerNav>
+            <NavbarStyle.Logo>
+              <Logo />
+            </NavbarStyle.Logo>
+            <NavbarStyle.ContainerMoreOptions onClick={toggleProfileMenu}>
+              <GrMenu />
+              <AvatarSVG />
+              {isOpen && widthSizeScreen >= 768 ? <ProfileMenu /> : ''}
+            </NavbarStyle.ContainerMoreOptions>
+          </NavbarStyle.ContainerNav>
+        </NavbarStyle.Nav>
+        {isOpen && widthSizeScreen <= 768 ? <ProfileMenu /> : ''}
+      </>
+    )
+}
