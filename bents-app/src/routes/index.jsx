43d0749899bf46a8e150be39b590c4e405a9b714
nachle <elias.nachle@bandtec.com.br>
2022-06-12 04:16:26 -0300

    feat: implementando progressBar em todas rotas privadas

--- bents-app/src/routes/index.jsx
+++ bents-app/src/routes/index.jsx
@@ -7,22 +7,6 @@ import SearchRestaurant from '../pages/searchRestaurant/SearchRestaurant'
 import Restaurant from '../pages/restaurant/Restaurant'
 import UserProflie from '../pages/user/userProfile/UserProfile'
 import { PrivateRoute } from './privateRoutes'
-import { paletteColors } from '../styles/Root'
-
-import ProgressBar from '@badrap/bar-of-progress'
-
-const progress = new ProgressBar({
-  size: 2.5,
-  color: paletteColors.primary,
-  className: 'z-50',
-  delay: 100
-})
-
-progress.start();
-
-setTimeout(() => {
-  progress.finish();
-}, 1000);
 
 export const AppRouter = () => {
   return (
@@ -30,7 +14,7 @@ export const AppRouter = () => {
       <Routes>
         <Route path="/*" element={<NotFound />} />
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login />} progress/>
         <Route path="/registro" element={<Register />} />
         <Route path="/restaurantes" element={<PrivateRoute />}>
           <Route path="/restaurantes" element={<SearchRestaurant />} />
