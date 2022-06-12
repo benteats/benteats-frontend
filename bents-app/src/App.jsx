528c1fffa4c97d98022c496a7a196a11861c29e9
nachle <elias.nachle@bandtec.com.br>
2022-06-12 03:53:56 -0300

    refactor: movendo progressbar

--- bents-app/src/App.jsx
+++ bents-app/src/App.jsx
@@ -1,20 +1,5 @@
 import { AuthProvider } from './context/AuthContext'
 import { AppRouter } from './routes'
-import { paletteColors } from './styles/Root'
-import ProgressBar from '@badrap/bar-of-progress'
-
-const progress = new ProgressBar({
-  size: 2.5,
-  color: paletteColors.primary,
-  className: 'z-50',
-  delay: 100
-})
-
-progress.start();
-
-setTimeout(() => {
-  progress.finish();
-}, 1000);
 
 export default function App() {
   return (
--- bents-app/src/routes/index.jsx
+++ bents-app/src/routes/index.jsx
@@ -5,7 +5,24 @@ import Login from '../pages/login/Login'
 import Register from '../pages/register/Register'
 import SearchRestaurant from '../pages/searchRestaurant/SearchRestaurant'
 import Restaurant from '../pages/restaurant/Restaurant'
+import UserProflie from '../pages/user/userProfile/UserProfile'
 import { PrivateRoute } from './privateRoutes'
+import { paletteColors } from '../styles/Root'
+
+import ProgressBar from '@badrap/bar-of-progress'
+
+const progress = new ProgressBar({
+  size: 2.5,
+  color: paletteColors.primary,
+  className: 'z-50',
+  delay: 100
+})
+
+progress.start();
+
+setTimeout(() => {
+  progress.finish();
+}, 1000);
 
 export const AppRouter = () => {
   return (
@@ -20,6 +37,9 @@ export const AppRouter = () => {
         </Route>
         <Route path="/restaurantes/restaurante/:id" element={<PrivateRoute />}>
           <Route path="/restaurantes/restaurante/:id" element={<Restaurant />} />
+        </Route>
+        <Route path="/preferencias" element={<PrivateRoute />}>
+          <Route path="/preferencias" element={<UserProflie />} />
         </Route>
       </Routes>
     </Router>
